1ae05a52adcc33248dbfc49b9a24b3b6ef61f714
anantharjun672001-a11y <anantharjun672001@gmail.com>
2025-12-06 19:22:24 +0530

    custom hook completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import UseReducer from './Components/UseReducer';
 import UseRef from './Components/UseRef';
+import Example from './Components/Custom Hook/Example';
 
 const App = () => {
   return (
     <div>
       {/* <UseReducer/> */}
-      <UseRef/>
-      
+      {/* <UseRef/> */}
+      <Example/>
+
     </div>
   );
 };
